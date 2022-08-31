4deb2f42feb4919e2c4783aa01481112066e0552
mwhy25 <mwhy25@gmail.com>
2022-08-31 08:55:50 +0700

    update

--- action.js
+++ action.js
@@ -4,17 +4,18 @@
 //     document.body.style.animationFillMode = "forwards";
 //     document.getElementById("buttclick").style.display = "block";
 // }, 5000);
-$("body").on("click", function() {
-    // $("#loading").remove();
+$("#loading").on("click", function() {
+    $("#loading").remove();
     $("#words>h1>span").each(function(i) {
         $(this).fadeIn(1000 * (i) * 0.5);
         // $(this).fadeIn().fadeOut(1000 * (i + 1));
     });
-    // $("h1>span").promise().done(function() {
-    //     $("#words").animate({
-    //         top: "-20%"
-    //     });
-    // });
+    $("#words>h1>span").promise().done(function() {
+        $("#words").animate({
+            top: "-20%";
+            position: "relative";
+        });
+    });
     // $("div").promise().done(function() {
     //     $("p").append(" Finished! ");
     // });
