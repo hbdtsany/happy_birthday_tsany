aadac2a2bfedc436a889b14df2581c84bdba61ad
mwhy25 <mwhy25@gmail.com>
2022-08-31 11:26:44 +0700

    curtain succees

--- action.js
+++ action.js
@@ -11,13 +11,41 @@ $("#loading").on("click", function() {
         // $(this).fadeIn().fadeOut(1000 * (i + 1));
     });
     $("#words>h1>span").promise().done(function() {
+        $("#words").css("position", "relative");
         $("#words").animate({
-            top: "-20%",
-            position: "relative",
+            top: "-30%",
         });
-        // $("#words").animate({});
+        $(".circle-btn").css("display", "block");
+        $(".circle-btn").fadeIn(2000);
     });
     // $("div").promise().done(function() {
     //     $("p").append(" Finished! ");
+    // });
+});
+$(".circle-btn").on("click", function() {
+    $("#words").fadeOut(1000);
+    $(".circle-btn").fadeOut(1000);
+    $(".circle-btn").promise().done(function() {
+        $("#words").remove()
+        $(".circle-btn").remove()
+        $("body").css("backgroundColor", "var(--pink)");
+        $(".blockleft").hide("slide", { direction: "left" }, 1000);
+        $(".blockright").hide("slide", { direction: "right" }, 1000, function() {
+            $("#curtain").remove();
+        });
+
+        // $(".blockleft").fadeOut(2000);
+        // $(".blockright").fadeOut(2000);
+    });
+    // $(".blockleft").promise().done(function() {
+    //     $("#curtain").remove();
+    // });
+
+    // $("#buttclick").promise().done(function() {
+    //     $("#buttclick").remove();
+    //     // document.body.style.backgroundColor = "var(--pink)";
+    //     $("body").css("backgroundColor", "var(--pink)");
+    //     $("#blockleft").addClass(["animate__animated", "animate__fadeOutLeft"]);
+    //     $("#blockright").addClass(["animate__animated", "animate__fadeOutRight"]);
     // });
 });
--- style.js
+++ style.js
@@ -7,14 +7,14 @@
 // }, 5000);
 
 function curtain() {
-    animateCSS('.blockleft', 'fadeOutLeft').then((message) => {
-        document.getElementById("curtain").remove();
-    });
-    animateCSS('.blockright', 'fadeOutRight').then((message) => {
-        // document.getElementById('curtain').remove();
-        document.getElementById("buttclick").remove();
-    });
+    // animateCSS('.blockleft', 'fadeOutLeft').then((message) => {
+    // document.getElementById("curtain").remove();
+    // });
+    // animateCSS('.blockright', 'fadeOutRight').then((message) => {
+    // document.getElementById('curtain').remove();
+    // document.getElementById("buttclick").remove();
+    // });
     // animateCSS('.middle', 'animate__fadeOut').then((message) => {
     // });
+    // document.body.style.backgroundColor = "var(--pink)";
 }
-// document.body.style.backgroundColor = "var(--pink)";
