74586205d52d3ea1a30b2e2febcb3748ca3a3fed
mwhy25 <mwhy25@gmail.com>
2022-08-31 16:21:47 +0700

    Opening Done

--- action.js
+++ action.js
@@ -1,14 +1,11 @@
-// setTimeout(function() {
-//     document.getElementById("loading").remove();
-//     document.body.style.animation = "bg 4s";
-//     document.body.style.animationFillMode = "forwards";
-//     document.getElementById("buttclick").style.display = "block";
-// }, 5000);
-$("#loading").on("click", function() {
+// Fungsi show/fadeIn otomatis ubah display none ke block
+$(".waviy").on("click", function() {
     $("#loading").remove();
+    $("#firework").fadeIn();
     $("#words>h1>span").each(function(i) {
+        // $(this).show("bounce", (1000 * (i) * 0.5));
+        // $(this).slideDown(1000 * (i) * 0.5);
         $(this).fadeIn(1000 * (i) * 0.5);
-        // $(this).fadeIn().fadeOut(1000 * (i + 1));
     });
     $("#words>h1>span").promise().done(function() {
         $("#words").css("position", "relative");
@@ -18,34 +15,38 @@ $("#loading").on("click", function() {
         $(".circle-btn").css("display", "block");
         $(".circle-btn").fadeIn(2000);
     });
-    // $("div").promise().done(function() {
-    //     $("p").append(" Finished! ");
-    // });
 });
 $(".circle-btn").on("click", function() {
     $("#words").fadeOut(1000);
+    $("#firework").fadeOut(1000);
     $(".circle-btn").fadeOut(1000);
     $(".circle-btn").promise().done(function() {
+        $("#firework").remove()
         $("#words").remove()
         $(".circle-btn").remove()
-        $("body").css("backgroundColor", "var(--pink)");
+        $("body").css("backgroundColor", "var(--light-blue)");
         $(".blockleft").hide("slide", { direction: "left" }, 1000);
         $(".blockright").hide("slide", { direction: "right" }, 1000, function() {
             $("#curtain").remove();
+            $("#card-menu").show("fold", 2000, showCongrats());
+            // $("#card-menu").show("fold", 2000, function() {
+            //     $("#congrats").show("bounce", 1000);
+            // });
+            // $("#congrats>h1>span").each(function(i) {
+            //     $(this).fadeIn(1000 * (i) * 0.5);
+            // });
         });
-
-        // $(".blockleft").fadeOut(2000);
-        // $(".blockright").fadeOut(2000);
     });
-    // $(".blockleft").promise().done(function() {
-    //     $("#curtain").remove();
-    // });
-
-    // $("#buttclick").promise().done(function() {
-    //     $("#buttclick").remove();
-    //     // document.body.style.backgroundColor = "var(--pink)";
-    //     $("body").css("backgroundColor", "var(--pink)");
-    //     $("#blockleft").addClass(["animate__animated", "animate__fadeOutLeft"]);
-    //     $("#blockright").addClass(["animate__animated", "animate__fadeOutRight"]);
+    // $("#card-menu").promise().done(function() {
+    //     $("#congrats").show("bounce", 1000);
     // });
 });
+
+function showCongrats() {
+    // $("#congrats").show("bounce", 4000);
+    $("#congrats").fadeIn(4000);
+    // $("#congrats>h1>span").each(function(i) {
+    //     $(this).fadeIn(1000 * (i) * 0.5);
+    // });
+    // $("#congrats").show("bounce", 4000);
+};
